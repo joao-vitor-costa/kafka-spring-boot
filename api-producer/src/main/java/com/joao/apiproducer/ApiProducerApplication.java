44c2241a9d85120581558fb9071911068d2297a1
João Vitor Costa <dev.es.costa@gmail.com>
2020-08-16 02:26:56 -0300

    Configurações

---
+++ api-producer/src/main/java/com/joao/apiproducer/ApiProducerApplication.java
@@ -0,0 +1,13 @@
+package com.joao.apiproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProducerApplication.class, args);
+	}
+
+}
---
+++ api-producer/src/test/java/com/joao/apiproducer/ApiProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.apiproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
